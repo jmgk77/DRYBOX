dd449e98b33e4a1db09a68d0feef79fbc3b9cb8d
JMGK (titan) <jmgk77@gmail.com>
2025-10-14 23:06:47 -0300

    readme

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "67"
+  #define BUILD_NUMBER "70"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.0.67 - 2025-10-14 22:40:18 [30ff51a|main]"
+  #define VERSION "v0.0.70 - 2025-10-14 22:45:57 [5d2b38e|main]"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.0.67"
+  #define VERSION_SHORT "v0.0.70"
 #endif
