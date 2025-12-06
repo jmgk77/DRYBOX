4eb884742f4a52ea9d379650522c04d7c4fb4ae2
JMGK (keres) <jmgk77@gmail.com>
2025-12-06 09:40:11 -0300

    exception decoder code; lwip2

---
+++ exception_decoder.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar EspStackTraceDecoder.jar /home/keres/.platformio/packages/toolchain-xtensa/bin/xtensa-lx106-elf-addr2line .pio/build/d1/firmware.elf /home/keres/Downloads/dump.txt
--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "436"
+  #define BUILD_NUMBER "439"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.0.436 - 2025-12-05 19:34:48 [d16c695|main]"
+  #define VERSION "v0.0.439 - 2025-12-06 09:31:11 [22eec3b|main]"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.0.436"
+  #define VERSION_SHORT "v0.0.439"
 #endif
